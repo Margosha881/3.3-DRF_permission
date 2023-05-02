9eeef719e258f30a2c9148af8b73a999bbfbd628
Margarita Gardt <margoritka881@mail.ru>
2023-05-02 20:15:20 +0300

    add files

---
+++ api_with_restrictions/advertisements/filters.py
@@ -0,0 +1,18 @@
+from django_filters import rest_framework as filters, DateFromToRangeFilter
+
+from advertisements.models import Advertisement, Favorite
+
+
+class AdvertisementFilter(filters.FilterSet):
+    """Фильтры для объявлений."""
+    created_at = DateFromToRangeFilter()
+
+    class Meta:
+        model = Advertisement
+        fields = ['creator', 'created_at',]
+
+
+class FavoriteFilter(filters.FilterSet):
+    class Meta:
+        model = Favorite
+        fields = ['person']
---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,38 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.filters import AdvertisementFilter, FavoriteFilter
+from advertisements.models import Advertisement, Favorite
+from advertisements.permissions import IsOwnerOrReadOnly, IsNotOwner
+from advertisements.serializers import AdvertisementSerializer, FavoriteSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    # permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AdvertisementFilter
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action == 'create':
+            return [IsAuthenticated()]
+        if self.action in ['update', 'partial_update', 'delete']:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        return []
+
+
+class FavoriteViewSet(ModelViewSet):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializer
+    # permission_classes = [IsAuthenticated, IsNotOwner]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = FavoriteFilter
+
+    def get_permissions(self):
+        if self.action in ['cretae','delete']:
+            return [IsAuthenticated(), IsNotOwner()]
+        return []
